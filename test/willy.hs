825f00e3542f75c87e21fda28f6c1a03fb1624ef
Luis Diaz <ldiazn98@gmail.com>
2020-01-30 21:54:20 -0400

    añadido makefile

--- test/willy.hs
+++ test/willy.hs
@@ -8,23 +8,25 @@ import System.Environment
 import System.Directory
 
 
+--  Este es el archivo main para la primera entrega, todas las funciones de impresiòn 
+--  y procesamiento de tokens estàn en Lexer.x, las definiciones de los token estàn en Tokens.hs
+
 main = do
     --Intenta recibir input:
-    
     inpt <- getArgs
 
     case inpt of     
-        (a:_) -> do
-            fileExists <- doesFileExist a
-            if fileExists then do
+        (a:_) -> do -- En caso de que hayas recibido al menos un argumento:
+            fileExists <- doesFileExist a   -- Coonsulta si el archivo existe
+            if fileExists then do           -- Si existe, compila
                 content <- readFile a
                 case tokenizer content of
                     Right tks -> putStr $ displayTokens tks
                     Left  s   -> putStr s
-            else do
+            else do                         -- Si no existe, imprime un error
                 putStrLn "Willy Error: El archivo dado no existe."
-        ([])  -> do
-            putStr "Nombre del archivo a compilar: "
+        ([])  -> do -- En caso de que  no hayan pasado ningùn argumento, pregunta por el archivo
+            putStrLn "Nombre del archivo a compilar: "
             a <- getLine
             fileExists <- doesFileExist a
             if fileExists then do
